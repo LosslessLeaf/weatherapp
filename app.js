325c31273d6610c28df8f823a63a6c93b5e9b9cd
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2019-01-27 21:25:11 +0000

    Added autoquery of position and weather update!

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 var express = require("express");
 var app = express();
+var request = require("request");
+var bodyParser = require("body-parser");
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
-    res.render(`home`);
+    res.render("search");
 });
 
-
 app.get("/weather", function(req, res) {
-    res.render("weather");
+    var lat = req.query.lat;
+    var long = req.query.long;
+    var url = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("weather", { data: data });
+        }
+    });
 });
 
 
